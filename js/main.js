758eed20d2ba2108fcb361d7fe171efcc1178cbb
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-11-27 17:00:38 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,46 +1,94 @@
+// Registrar a Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
       let reg;
-      reg = await navigator.serviceWorker.register('./sw.js', {type: "module"});
+      reg = await navigator.serviceWorker.register('./sw.js', { type: 'module' });
       console.log('Service worker registrada!', reg);
     } catch (err) {
       console.log(' Service worker registro falhou:', err);
     }
   });
-};
+}
 
-let posicaoInicial; //variavel para capturar a posicao
-const capturarLocalizacao = document.getElementById('localizacao');
-const latitude = document.getElementById('latitude');
-const longitude = document.getElementById('longitude');
-const map = document.getElementById('mapa');
-const sucesso = (posicao) => {//callback de sucesso para captura da posicao
-  posicaoInicial = posicao;
-  latitude.innerHTML = posicaoInicial.coords.latitude;
-  longitude.innerHTML = posicaoInicial.coords.longitude;
-  map.src = "https://maps.google.com/maps?q=" + posicaoInicial.coords.latitude + ", " + posicaoInicial.coords.longitude + "&z=16&output=embed"
+// Obter referências aos elementos do formulário e do mapa
+var cepInput = document.getElementById("cep");
+var ruaInput = document.getElementById("rua");
+var numeroInput = document.getElementById("numero");
+var bairroInput = document.getElementById("bairro");
+var cidadeInput = document.getElementById("cidade");
+var estadoInput = document.getElementById("estado");
+
+var map = document.getElementById('mapa');
+
+document.getElementById("salvarBtn").addEventListener("click", function() {
+  SalvarDados();
+});
+
+function SalvarDados() {
+
+  var cep = cepInput.value;
+  var rua = ruaInput.value;
+  var numero = numeroInput.value;
+  var bairro = bairroInput.value;
+  var cidade = cidadeInput.value;
+  var estado = estadoInput.value;
+
+  console.log("Dados salvos:", {
+    cep: cep,
+    rua: rua,
+    numero: numero,
+    bairro: bairro,
+    cidade: cidade,
+    estado: estado
+  });
+
+
+  AtualizarMapa(cep, rua, numero, cidade, estado);
+}
+
+function AtualizarMapa(cep, rua, numero, cidade, estado) {
+  
+  map.src = "https://maps.google.com/maps?q=" + rua + "+" + numero + "%2C+" + cidade + "+" + estado + "+" + cep + "&t=&z=13&ie=UTF8&iwloc=&output=embed";
+
+
+  if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage({
+      type: 'saveData',
+      data: {
+        cep: cep,
+        rua: rua,
+        numero: numero,
+        bairro: bairro,
+        cidade: cidade,
+        estado: estado
+      }
+    });
+  }
 }
 
 
-const erro = (error) => {//callback ite error (falha para captura de Localizacao)
+
+
+const erro = (error) => {
   let errorMessage;
-  switch(error.code){
+  switch (error.code) {
     case 0:
-      errorMessage = "Erro desconhecido"
-    break;
+      errorMessage = "Erro desconhecido";
+      break;
     case 1:
-      errorMessage = "Permissão negada!"
-    break;
+      errorMessage = "Permissão negada!";
+      break;
     case 2:
-      errorMessage = "Captura de posição indisponivel!"
-    break;
+      errorMessage = "Captura de posição indisponível!";
+      break;
     case 3:
-      errorMessage = "Tempo de solicitação excedido!"
-    break ;
+      errorMessage = "Tempo de solicitação excedido!";
+      break;
   }
   console.log('Ocorreu um erro: ' + errorMessage);
-}
+};
+
 capturarLocalizacao.addEventListener('click', () => {
   navigator.geolocation.getCurrentPosition(sucesso, erro);
 });
